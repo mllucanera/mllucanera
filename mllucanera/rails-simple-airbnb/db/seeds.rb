169ed22abbd3029d0e164c109b517cd817931754
maria laura lucanera <mllucanera@gmail.com>
2018-07-27 14:38:56 -0300

    Old stuff

---
+++ mllucanera/rails-simple-airbnb/db/seeds.rb
@@ -0,0 +1,39 @@
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Nice Flat Paris',
+  address: 'Rheu 23',
+  description: 'Lovely warm comfortable and stylishly furnished house. Private bedroom and bathroom with shared living areas.',
+  price_per_night: 50,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Schoon flat in Berlin',
+  address: 'Legeweg 405',
+  description: 'A mini apartment within the clock tower apartment at St Pancras Station, with its own en suite bathroom and kitchen and sitting area.',
+  price_per_night: 45,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Bello apartamento a Roma',
+  address: 'Da Vinci 356',
+  description: 'Apartment situated below the fortress Spanjola with the central position and view on the beautiful Hvar harbour and Pakleni islands.',
+  price_per_night: 60,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Pequeño y simpático departamento en Buenos Aires',
+  address: 'Ecuador 1588',
+  description: 'Un 100 % de los huéspedes que se han hospedado aquí recientemente ha valorado la ubicación de este alojamiento con 5 estrellas.',
+  price_per_night: 35,
+  number_of_guests: 3
+)
